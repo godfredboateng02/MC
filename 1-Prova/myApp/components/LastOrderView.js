241421c959deb5c7014e746b91b57eaa4efefff0
Godfred <godfred.boateng@studenti.unimi.it>
2025-02-16 22:26:50 +0100

    Ombra

--- 1-Prova/myApp/components/LastOrderView.js
+++ 1-Prova/myApp/components/LastOrderView.js
@@ -58,6 +58,11 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
         alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 3,
     },
     cardImage: {
         width: 100, // Imposta la larghezza dell'immagine
--- 1-Prova/myApp/screens/Profile.js
+++ 1-Prova/myApp/screens/Profile.js
@@ -301,12 +301,5 @@ const styles = StyleSheet.create({
 
     touchableOrderConteiner: {
         width: '100%',
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        elevation: 5,
     }
-
-    
 });
